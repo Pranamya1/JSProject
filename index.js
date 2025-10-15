8343534ee05469ddc3a003fee87ea0e507dbe52a
Pranamya_FullStackDevloper <pranamya653@gmail.com>
2025-10-15 18:48:26 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-// sliding of sidebar in sml scr
-const mobSidebarBtn = document.getElementById('mob_sidebar_slid-btn');
-const mobSidebar = document.querySelector('.mob-sidebar');
-
-mobSidebarBtn.addEventListener('click', () => {
-    mobSidebar.classList.toggle('active'); // toggle sidebar visibility
-});
